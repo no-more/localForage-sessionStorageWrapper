84226e217e7965bef09656dbd57616ee43a0bc1b
Christophe HOARAU <no-more@users.noreply.github.com>
2018-01-20 18:53:48 +0400

    Update localforage-sessionstoragewrapper.js
    Fix error with webpack

--- src/localforage-sessionstoragewrapper.js
+++ src/localforage-sessionstoragewrapper.js
@@ -87,14 +87,14 @@
 
             // We allow localForage to be declared as a module or as a
             // library available without AMD/require.js.
-            if (moduleType === ModuleType.DEFINE) {
-                require(['localforageSerializer'], resolve);
-            } else if (moduleType === ModuleType.EXPORT) {
-                // Making it browserify friendly
-                resolve(require('./../utils/serializer'));
-            } else {
+//             if (moduleType === ModuleType.DEFINE) {
+//                 require(['localforageSerializer'], resolve);
+//             } else if (moduleType === ModuleType.EXPORT) {
+//                 // Making it browserify friendly
+//                 resolve(require('./../utils/serializer'));
+//             } else {
                 resolve(globalObject.localforageSerializer);
-            }
+//             }
         });
 
         return serializerPromise.then(function(lib) {
